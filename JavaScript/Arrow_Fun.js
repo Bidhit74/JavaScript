8ea99eb4ab9bae0ec45e7bcaa07c7ef688de9bb7
Bidhit74 <143928281+Bidhit74@users.noreply.github.com>
2023-09-16 17:10:34 +0000

    New Commit

---
+++ JavaScript/Arrow_Fun.js
@@ -0,0 +1,47 @@
+// THIS and arrow function
+
+// let user = {
+//     username: "Bidhit",
+//     age: 22,
+//     welcomeMessage: function () {
+//         console.log(`Namaste ${this.username}, welcome to my website`);
+//         // console.log(this);
+//     }
+// }
+
+// user.welcomeMessage();
+// user.username = "Narayan";
+// user.welcomeMessage();
+// console.log(this);
+
+// function chai() {
+//     console.log(this);
+// }
+// chai();
+
+// let user = function () {
+//     let username = "Narayan";
+//     console.log(this.username);
+// }
+
+// let user = () => {
+//     let username = "Narayan";
+//     console.log(this);
+// }
+// user();
+
+// arrow function: () => {}
+
+// let addTwo = (num1, num2) => {
+//     return num1 + num2;
+// }
+// OR
+// let addTwo = (num1, num2) => num1 + num2;
+// OR
+let addTwo = (num1, num2) => (num1 + num2);
+
+// let addTwo = (num1, num2) => {username: "chiranjeev"};
+// let addTwo = (num1, num2) => ({username: "chiranjeev"});
+console.log(addTwo(1, 8));
+console.log(addTwo(9, 8));
+
