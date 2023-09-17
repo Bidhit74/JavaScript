892eff68a7122674c9a25afdf63c4ea5e68677aa
Bidhit74 <143928281+Bidhit74@users.noreply.github.com>
2023-09-17 15:43:00 +0000

    ForEach loops in Array.

---
+++ JavaScript/iterations/forEach.js
@@ -0,0 +1,51 @@
+// ================= ForEach loops in Array. ===================
+
+// const coding = ["js", "ruby", "java", "python", "cpp"];
+
+// coding.forEach(function (val) {
+//   console.log(val);
+// });
+
+// // coding.forEach( (item) => {
+// //     console.log(item);
+// // } )
+
+// function printMe(item){
+//     console.log(item);
+// }
+// coding.forEach(printMe); // only reference
+
+// coding.forEach((item, index, arr) => {
+//   console.log(item, index, arr);
+// });
+
+// const myCoding = [
+//   {
+//     languageName: "javascript",
+//     languageFileName: "js",
+//   },
+//   {
+//     languageName: "java",
+//     languageFileName: "java",
+//   },
+//   {
+//     languageName: "python",
+//     languageFileName: "py",
+//   }
+// ];
+
+// myCoding.forEach((item) => {
+//   // console.log(item.languageName);
+//   console.log(item);
+// });
+
+// ================= ForEach loops in Array. ===================
+
+// const coding = ["js", "ruby", "java", "python", "cpp"]
+
+// const values = coding.forEach( (item) => {
+//     //console.log(item);
+//   return item;
+// } )
+
+// console.log(values); // ForEach Anyone value not return this is return undefined
